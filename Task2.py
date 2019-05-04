7706080f8ee46fbe79aea7381161a96663ca09ea
Andy Singh <andy.singh@usc.edu>
2019-05-04 15:25:14 -0700

    Implemented Task3

--- Task2.py
+++ Task2.py
@@ -20,3 +20,58 @@ Print a message:
 September 2016.".
 """
 
+# Choosing to use a dictionary where the key is the phone number and the total talk time is the value
+# Pseudocode
+# For each entry in the call log
+#   If key is in dictionary
+#       Add to existing time
+#   Else If key is not in dictionary
+#       Add new entry into dictionary
+#   Compare to longest time
+#   If greater than current longest time, set as new longest time
+# Print out message
+
+# Phone call column definitions
+col_callFrom = 0
+col_callTo = 1
+col_callStartAt = 2
+col_callDuration = 3
+
+callTimes = dict()
+longestTimePhoneNumber = calls[0][0]  # Setting the initial value as the first number
+
+# Iterating over the call entries -> O(n)
+for entry in calls:
+    # Get the phone numbers for this entry and how long they were talking
+    callFromPhoneNumber = entry[col_callFrom]
+    callToPhoneNumber = entry[col_callTo]
+    callDuration = entry[col_callDuration]
+
+    # Check if the caller's phone number is already in the dictionary
+    # If so, add the current call to the total duration
+    # Checking for key in dictionary -> O(1)
+    # Setting value in dictionary -> O(1)
+    if callFromPhoneNumber in callTimes:
+        callTimes[callFromPhoneNumber] += int(callDuration)
+    else:
+        callTimes[callFromPhoneNumber] = int(callDuration)
+
+    # Check if sum of the call duration is now the longest
+    # Checking values in dictionary -> O(1)
+    if callTimes[callFromPhoneNumber] > callTimes[longestTimePhoneNumber]:
+        longestTimePhoneNumber = callFromPhoneNumber
+
+    # Now check for the phone number of the person being called
+    if callToPhoneNumber in callTimes:
+        callTimes[callToPhoneNumber] += int(callDuration)
+    else:
+        callTimes[callToPhoneNumber] = int(callDuration)
+
+    # Check if sum of the call duration is now the longest
+    if callTimes[callToPhoneNumber] > callTimes[longestTimePhoneNumber]:
+        longestTimePhoneNumber = callToPhoneNumber
+
+print("%s spent the longest time, %s seconds, on the phone during September 2016." %
+      (longestTimePhoneNumber, callTimes[longestTimePhoneNumber]))
+
+# Overall time complexity -> O(n)
--- Task3.py
+++ Task3.py
@@ -57,3 +57,70 @@ The percentage should have 2 decimal digits
 #       If called number had 080 area code (isBangloreNumber), increment sumOfBangloreNumbersCalled
 # Print Message 1
 # Print Message 2
+
+# Phone call column definitions
+col_callFrom = 0
+col_callTo = 1
+col_callStartAt = 2
+col_callDuration = 3
+
+
+# Extract the codes and prefixes from phone numbers based on the conventions outlined above
+def get_codes_and_prefixes(phone_number):
+    # Case 1: Is it a fixed line? (Has area code enclosed in parenthesis)
+    # Will look up whether phone number has open parenthesis
+    if '(' in phone_number:
+        # Return the portion of the string between the parenthesis
+        return phone_number.split(')')[0][1:]
+
+    # Case 2: Is the phone number a mobile number? (No parenthesis but has space in middle)
+    # Will check if phone number contains space
+    elif ' ' in phone_number:
+        # Return the portion of the string before the space
+        return phone_number.split(' ')[0]
+
+    # Case 3: Otherwise it's a telemarketer (with a 140 prefix)
+    else:
+        return '140'
+
+
+# Figure out if the phone number comes from a Bangalore fixed line (has 080 area code)
+# Time complexity: O(1)
+def is_bangalore_number(phone_number):
+    return phone_number[:5] == '(080)'
+
+
+# Testing
+# print(get_codes_and_prefixes('78130 00821'))    # Expected: 78130
+# print(get_codes_and_prefixes('(080)33118033'))  # Expected: 080
+# print(get_codes_and_prefixes('1408371942'))     # Expected: 140
+# print(get_codes_and_prefixes('(04344)322628'))  # Expected: 04344
+#
+# print(is_bangalore_number('78130 00821'))    # Expected: False
+# print(is_bangalore_number('(080)33118033'))  # Expected: True
+# print(is_bangalore_number('1408371942'))     # Expected: False
+# print(is_bangalore_number('(04344)322628'))  # Expected: False
+
+codesAndPrefixesCalled = set()
+numOfBangaloreNumbersCalled = 0
+
+# Time complexity to iterate through call entries O(n)
+for entry in calls:
+    call_from = entry[col_callFrom]
+    call_to = entry[col_callTo]
+
+    if is_bangalore_number(call_from):
+        codesAndPrefixesCalled.add(get_codes_and_prefixes(call_to))
+        if is_bangalore_number(call_to):
+            numOfBangaloreNumbersCalled += 1
+
+
+# Output for part 1:
+print("The numbers called by people in Bangalore have codes:")
+for code in sorted(codesAndPrefixesCalled):
+    print(code)
+
+# Output for Part 2:
+percentOfBangaloreCalls = '{0:.2f}'.format(100 * numOfBangaloreNumbersCalled/len(calls))
+print("%s percent of calls from fixed lines in Bangalore are calls to other fixed lines in Bangalore."
+      % percentOfBangaloreCalls)
